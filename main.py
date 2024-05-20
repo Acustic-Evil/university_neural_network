f17b9da35a6d1943de671e843f00c8f60ed81177
Acustic_Evil <prisha931@gmail.com>
2024-05-20 03:12:17 +0300

    initial commit with working neural network

---
+++ main.py
@@ -0,0 +1,138 @@
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from tensorflow import keras
+
+from keras.models import Sequential
+
+model = Sequential()
+
+
+def neural_training():
+    data = []
+    labels = []
+    many_deer = os.listdir("deer")
+    for deer in many_deer:
+        imag = cv2.imread("deer/" + deer)
+        img_from_ar = Image.fromarray(imag, 'RGB')
+        resized_image = img_from_ar.resize((50, 50))
+        data.append(np.array(resized_image))
+        labels.append(0)
+
+    cats = os.listdir("cats")
+    for cat in cats:
+        imag = cv2.imread("cats/" + cat)
+        img_from_ar = Image.fromarray(imag, 'RGB')
+        resized_image = img_from_ar.resize((50, 50))
+        data.append(np.array(resized_image))
+        labels.append(1)
+
+    dogs = os.listdir("dogs")
+    for dog in dogs:
+        imag = cv2.imread("dogs/" + dog)
+        img_from_ar = Image.fromarray(imag, 'RGB')
+        resized_image = img_from_ar.resize((50, 50))
+        data.append(np.array(resized_image))
+        labels.append(2)
+
+    animals = np.array(data)
+    labels = np.array(labels)
+
+    np.save("animals", animals)
+    np.save("labels", labels)
+
+    animals = np.load("animals.npy")
+    labels = np.load("labels.npy")
+
+    s = np.arange(animals.shape[0])
+    np.random.shuffle(s)
+    animals = animals[s]
+    labels = labels[s]
+
+    num_classes = len(np.unique(labels))
+    data_length = len(animals)
+
+    (x_train, x_test) = animals[(int)(0.1 * data_length):], animals[:(int)(0.1 * data_length)]
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+    train_length = len(x_train)
+    test_length = len(x_test)
+
+    (y_train, y_test) = labels[(int)(0.1 * data_length):], labels[:(int)(0.1 * data_length)]
+
+    # One hot encoding
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    # import sequential model and all the required layers
+    from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+    # make model
+    model.add(Conv2D(filters=16, kernel_size=2, padding="same", activation="relu", input_shape=(50, 50, 3)))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=32, kernel_size=2, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=64, kernel_size=2, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(500, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Dense(3, activation="softmax"))
+    model.summary()
+
+    # compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(x_train, y_train, batch_size=50, epochs=100, verbose=1)
+
+    score = model.evaluate(x_test, y_test, verbose=1)
+    print('\n', 'Test accuracy:', score[1])
+
+    from keras.models import model_from_json
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
+
+def convert_to_array(img):
+    im = cv2.imread(img)
+    img = Image.fromarray(im, 'RGB')
+    image = img.resize((50, 50))
+    return np.array(image)
+
+
+def get_animal_name(label):
+    if label == 0:
+        return "deer"
+    if label == 1:
+        return "cat"
+    if label == 2:
+        return "dog"
+
+
+def predict_animal(file):
+    print("Predicting .................................")
+    ar = convert_to_array(file)
+    ar = ar / 255
+    label = 1
+    a = []
+    a.append(ar)
+    a = np.array(a)
+    prediction_score = model.predict(a, verbose=1)
+    print(prediction_score)
+    label_index = np.argmax(prediction_score)
+    print(label_index)
+    acc = np.max(prediction_score)
+    animal = get_animal_name(label_index)
+    print(animal)
+    print("The predicted Animal is a " + animal + " with accuracy =    " + str(acc))
+
+
+if __name__ == '__main__':
+    predict_animal('./deer/0a36a364ce.jpg')
