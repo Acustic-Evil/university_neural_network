03bab3918971db2dc45ddfc2a30d4b3f4607949e
Acustic_Evil <prisha931@gmail.com>
2024-05-20 15:19:16 +0300

    took out model training into separate file, added check if model exists and loading the model

--- main.py
+++ main.py
@@ -2,102 +2,27 @@ import os
 import cv2
 from PIL import Image
 import numpy as np
-from tensorflow import keras
 
-from keras.models import Sequential
+from keras.models import model_from_json
 
-model = Sequential()
+from neural_training import neural_training
 
 
-def neural_training():
-    data = []
-    labels = []
-    many_deer = os.listdir("deer")
-    for deer in many_deer:
-        imag = cv2.imread("deer/" + deer)
-        img_from_ar = Image.fromarray(imag, 'RGB')
-        resized_image = img_from_ar.resize((50, 50))
-        data.append(np.array(resized_image))
-        labels.append(0)
+# Function to check if model files exist
+def check_model_files():
+    return os.path.exists('model.json') and os.path.exists('model.h5')
 
-    cats = os.listdir("cats")
-    for cat in cats:
-        imag = cv2.imread("cats/" + cat)
-        img_from_ar = Image.fromarray(imag, 'RGB')
-        resized_image = img_from_ar.resize((50, 50))
-        data.append(np.array(resized_image))
-        labels.append(1)
 
-    dogs = os.listdir("dogs")
-    for dog in dogs:
-        imag = cv2.imread("dogs/" + dog)
-        img_from_ar = Image.fromarray(imag, 'RGB')
-        resized_image = img_from_ar.resize((50, 50))
-        data.append(np.array(resized_image))
-        labels.append(2)
+# Function to load the model from the JSON and H5 files
+def load_model():
+    # Load the model architecture from the JSON file
+    with open('model.json', 'r') as json_file:
+        model_json = json_file.read()
+    model = model_from_json(model_json)
 
-    animals = np.array(data)
-    labels = np.array(labels)
-
-    np.save("animals", animals)
-    np.save("labels", labels)
-
-    animals = np.load("animals.npy")
-    labels = np.load("labels.npy")
-
-    s = np.arange(animals.shape[0])
-    np.random.shuffle(s)
-    animals = animals[s]
-    labels = labels[s]
-
-    num_classes = len(np.unique(labels))
-    data_length = len(animals)
-
-    (x_train, x_test) = animals[(int)(0.1 * data_length):], animals[:(int)(0.1 * data_length)]
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-    train_length = len(x_train)
-    test_length = len(x_test)
-
-    (y_train, y_test) = labels[(int)(0.1 * data_length):], labels[:(int)(0.1 * data_length)]
-
-    # One hot encoding
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    # import sequential model and all the required layers
-    from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-
-    # make model
-    model.add(Conv2D(filters=16, kernel_size=2, padding="same", activation="relu", input_shape=(50, 50, 3)))
-    model.add(MaxPooling2D(pool_size=2))
-    model.add(Conv2D(filters=32, kernel_size=2, padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=2))
-    model.add(Conv2D(filters=64, kernel_size=2, padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=2))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(500, activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Dense(3, activation="softmax"))
-    model.summary()
-
-    # compile the model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    model.fit(x_train, y_train, batch_size=50, epochs=100, verbose=1)
-
-    score = model.evaluate(x_test, y_test, verbose=1)
-    print('\n', 'Test accuracy:', score[1])
-
-    from keras.models import model_from_json
-    # serialize model to JSON
-    model_json = model.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights("model.h5")
-    print("Saved model to disk")
+    # Load the weights into the model
+    model.load_weights('model.h5')
+    return model
 
 
 def convert_to_array(img):
@@ -117,7 +42,13 @@ def get_animal_name(label):
 
 
 def predict_animal(file):
+    # Check if model files exist, if not, start training
+    if not check_model_files():
+        print("Model files not found, starting training...")
+        neural_training()
+
     print("Predicting .................................")
+    model = load_model()
     ar = convert_to_array(file)
     ar = ar / 255
     label = 1
@@ -135,4 +66,5 @@ def predict_animal(file):
 
 
 if __name__ == '__main__':
-    predict_animal('./deer/0a36a364ce.jpg')
+    predict_animal('./deer/0bdd8b2ba3.jpg')
+    predict_animal('./dogs/dog.4001.jpg')
