dac787274db8a0613d6e16b8011145a246c2a81d
Acustic_Evil <prisha931@gmail.com>
2024-05-20 17:59:44 +0300

    reformated for pip8

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, Label, Button
 import neural_training
 
+
 # Function to check if model files exist
 def check_model_files():
     return os.path.exists('model.json') and os.path.exists('model.h5')
+
 
 # Function to load the model from the JSON and H5 files
 def load_model():
@@ -23,11 +25,13 @@ def load_model():
     model.load_weights('model.h5')
     return model
 
+
 def convert_to_array(img):
     im = cv2.imread(img)
     img = Image.fromarray(im, 'RGB')
     image = img.resize((50, 50))
     return np.array(image)
+
 
 def get_animal_name(label):
     if label == 0:
@@ -36,6 +40,7 @@ def get_animal_name(label):
         return "cat"
     if label == 2:
         return "dog"
+
 
 def predict_animal(file):
     # Check if model files exist, if not, start training
@@ -54,6 +59,7 @@ def predict_animal(file):
     animal = get_animal_name(label_index)
     return animal, acc
 
+
 def upload_image():
     file_path = filedialog.askopenfilename()
     if not file_path:
@@ -61,16 +67,18 @@ def upload_image():
     animal, acc = predict_animal(file_path)
     display_result(file_path, animal, acc)
 
+
 def display_result(image_path, animal, acc):
     load = Image.open(image_path)
     render = ImageTk.PhotoImage(load)
-    
+
     img = Label(image=render)
     img.image = render
     img.grid(column=1, row=1, padx=10, pady=10)
 
     result_text.set(f"The predicted Animal is a {animal} with accuracy = {acc}")
     result_label.grid(column=1, row=2, padx=10, pady=10)
+
 
 def create_gui():
     global result_text, result_label
@@ -85,6 +93,7 @@ def create_gui():
     result_label = Label(root, textvariable=result_text, font=("Helvetica", 16))
 
     root.mainloop()
+
 
 if __name__ == '__main__':
     create_gui()
